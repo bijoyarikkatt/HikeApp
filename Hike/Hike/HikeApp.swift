cc77846b13d483fc566d48b83384907521e65ded
Bijoy Arikkatt Babu <bijoyarikkatt@gmail.com>
2024-11-04 12:42:47 +0000

    Initial commit

---
+++ Hike/Hike/HikeApp.swift
@@ -0,0 +1,17 @@
+//
+//  HikeApp.swift
+//  Hike
+//
+//  Created by Bijoy A B on 04/11/24.
+//
+
+import SwiftUI
+
+@main
+struct HikeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
